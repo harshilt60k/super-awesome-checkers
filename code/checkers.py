acfcbc401a34523fb69fcc3ba87e5d20bd9b5d0b
Jake Dreher <jjd358@tux3.cci.drexel.edu>
2021-02-11 21:28:48 -0500

    I'm working on the rules using a board in the terminal

---
+++ code/checkers.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 11 21:14:09 2021
+
+@author: Jake
+"""
+import pprint
+pp=pprint.PrettyPrinter(width=14, compact=True)
+board=[ 0 for x in range(0,32)]
+for i in range(0,12):
+    board[i]=1
+    
+for i in range(20,32):
+    board[i]=1
+
+
