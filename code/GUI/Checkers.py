20b93aa41c7538c0e076dd1c0a3c284060cbb208
Dubem Okoye <2686-do427@users.noreply.gitlab.cci.drexel.edu>
2021-03-08 23:48:27 +0000

    Replace Checkers.py

--- code/GUI/Checkers.py
+++ code/GUI/Checkers.py
@@ -1,5 +1,6 @@
 import pygame
 import pyautogui
+from os import path
 pygame.init()
 scrnw = int(1920)
 scrnh = int(1080)
@@ -76,23 +77,30 @@ def pieces(x = 0, y = 0):
             bx = 0
             by = by + size
 
+def makeButtons():
+    global buttons
+    buttons = []
+    buttons.append(button('undo', 0, 8))
+    buttons.append(button('redo', 1, 8))
+    buttons.append(button('settings', 7, 8))
+        
 def updatePieces(x = 0, y = 0):
     global size
     size = int(boardsize/8)
     i = 0
-    while i < 12:
-        pc = checkers[i]
-        pc.setX(pc.getX())
-        pc.setY(pc.getY())
-        pc.show()
-        i = i + 1
     while i < 24:
         pc = checkers[i]
-        pc.setX(pc.getX())
-        pc.setY(pc.getY())
+        pc.setX()
+        pc.setY()
         pc.show()
         i = i + 1
-    pygame.display.update()
+    i = 0
+    while i < 3:
+        pc = buttons[i]
+        pc.setX()
+        pc.setY()
+        pc.show()
+        i = i + 1
 def background():
     screen.blit(felt, (0,0))
     drawboard((scrnw-boardsize)/2,(scrnh-boardsize)/2)
@@ -106,6 +114,7 @@ def load():
     global blackprsn
     global pointer
     global boardsize
+    pygame.font.init()
     felt = pygame.image.load(r'greenfelt.jpg')
     felt = pygame.transform.smoothscale(felt, (scrnw, scrnh))
     frame = pygame.image.load(r'GoldFrameTransparent.png')
@@ -115,12 +124,106 @@ def load():
     pointer = pygame.image.load(r'pointer.png')
     boardsize = int(scrnh*4/5)
 
-class board():
-    pass
+class button():
+    def __init__(self, name, xspot, yspot = 8):
+        self.xspot = xspot
+        self.yspot = yspot
+        self.name = name
+        self.selected = False
+        if path.exists(r'' + name + '.png'):
+            self.pc = pygame.image.load(r'' + name + '.png')
+        else:
+            self.pc = pygame.image.load(r'' + name + '.jpg')
+
+    def do(self):
+        if self.name == 'undo':
+            pass
+        if self.name == 'redo':
+            pass
+        if self.name == 'settings':
+            while True:
+                wh = pygame.display.get_window_size()
+                scrnw = int(wh[0])
+                scrnh = int(wh[0]*9/16)
+                screen = pygame.display.set_mode((scrnw,scrnh), pygame.RESIZABLE)
+                xx = (scrnw-boardsize)/2
+                yy = (scrnh-boardsize)/2
+                load()
+                background()
+                updatePieces()
+                column = 0
+                lessr = 153/(boardsize)
+                r = 153
+                lessg = 179/(boardsize)
+                g = 179
+                moreb = (255 - 237)/(boardsize)
+                b = 237
+                while column < boardsize + 1:
+                    pygame.draw.line(screen, (r,g,b), (xx,yy+column), (xx+boardsize,yy+column), 1)
+                    r = r - lessr
+                    g = g - lessg
+                    b = b + moreb
+                    column = column + 1
+                afont = pygame.font.SysFont('microsoftyaheimicrosoftyaheiui', int(scrnh/30))
+                text = afont.render('Themes', True, (0, 0, 0))
+                screen.blit(text, (xx, yy))
+                for event in pygame.event.get():
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        mouse = pygame.mouse.get_pos()
+                        for i in buttons:
+                            if i.getX() <= mouse[0] < i.getX()+size and i.getY() <= mouse[1] < i.getY()+size:
+                                choice = i
+                                for j in buttons:
+                                    if j.getSelect() and j != choice:
+                                        j.select()
+                                i.select()
+                                if i == self:
+                                    self.selected = False
+                                    return
+                                i.do()
+                pygame.display.update()
+                
+        self.selected = False
+
+    def getSelect(self):
+        return self.selected
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def setX(self):
+        self.x = xx + self.xspot*size
+
+    def setY(self):
+        self.y = yy + self.yspot*size
+
+    def select(self):
+        self.selected = True
+
+    def show(self):
+        length = int(scrnh/10*0.7)
+
+        if self.selected:
+            if path.exists(r'' + self.name + 'blue.png'):
+                self.pc = pygame.image.load(r'' + self.name + 'blue.png')
+            else:
+                self.pc = pygame.image.load(r'' + self.name + 'blue.jpg')
+        else:
+            if path.exists(r'' + self.name + '.png'):
+                self.pc = pygame.image.load(r'' + self.name + '.png')
+            else:
+                self.pc = pygame.image.load(r'' + self.name + '.jpg')
+        self.pc = pygame.transform.smoothscale(self.pc, (length,length))
+        screen.blit(self.pc, (self.x + int(length/0.7 - length)/2, self.y + int((length/0.7 - length)/2)))
 
 class piece():
 
     number = 0
+    redeaten = 0
+    blackeaten = 0
 
     def __init__(self, x, y, xspot, yspot, color):
         self.pc = pygame.image.load(r'' + color + '.png')
@@ -133,6 +236,7 @@ class piece():
         self.king = False
         self.eaten = False
         self.selected = False
+        self.timeeaten = None
         piece.number = piece.number + 1
         self.number = piece.number
         screen.blit(self.pc, (self.x,self.y))
@@ -143,17 +247,76 @@ class piece():
     def getY(self):
         return self.y
 
-    def setX(self, x):
-##        self.x = xx + self.xspot*size
-        self.x = x
+    def makeKing(self):
+        self.king = True
+        self.pc = pygame.image.load(r'' + self.color + 'king.png')
+        self.pc = pygame.transform.smoothscale(self.pc, (int(scrnh/10),int(scrnh/10)))
 
-    def setY(self, y):
-##        self.y = yy + self.yspot*size
-        self.y = y
+    def setX(self):
+        if not self.eaten:
+            self.x = xx + self.xspot*size
+        else:
+            inby = int(scrnh/17)
+            if self.color == 'red':
+                self.x = (int(((scrnw-boardsize)/2-frame.get_width())/2) + inby) + (int(scrnh*3/6) - inby)/3*(self.timeeaten % 3)
+            if self.color == 'black':
+                self.x = (int(((scrnw-(scrnw-boardsize)/2)+scrnw)/2-frame.get_width()/2) + inby) + (int(scrnh*3/6) - inby)/3*(self.timeeaten % 3)
+
+    def setY(self):
+        if not self.eaten:
+            self.y = yy + self.yspot*size
+        else:
+            inby = int(scrnh/17)
+            if self.color == 'red':
+                self.y = (0 + inby) + (int(scrnh*3/6*(116/81)) - inby*2)/4*int(self.timeeaten / 3)
+            if self.color == 'black':
+                self.y = (0 + inby) + (int(scrnh*3/6*(116/81)) - inby*2)/4*int(self.timeeaten / 3)
 
     def getSelect(self):
         return self.selected
 
+    def eat(self):
+        self.eaten = True
+        self.y = 0
+        self.x = 0
+        self.yspot = -2
+        self.xspot = -2
+        if self.color == 'red':
+            self.timeeaten = piece.redeaten
+            piece.redeaten += 1
+
+        elif self.color == 'black':
+            self.timeeaten = piece.blackeaten
+            piece.blackeaten += 1
+
+    def motion(self, oldx, oldy, chngsize):
+        rect = pygame.Rect(xx, yy, boardsize, boardsize)
+        board = screen.subsurface(rect)
+        pygame.image.save(board, r'board.png')
+        board = pygame.image.load(r'board.png')
+        xf = 0
+        yf = 0
+        x = self.x - oldx
+        y = self.y - oldy
+        speed = 50
+        grow = 3
+        i = 0
+        while True:
+            screen.blit(board, (xx, yy))
+            pygame.display.update()
+            xf = xf + x/speed
+            yf = yf + y/speed
+            self.pc = pygame.image.load(r'' + self.color + 'king'*self.king + '.png')
+            self.pc = pygame.transform.smoothscale(self.pc, (int(scrnh/10+(-abs(i-speed/2)+speed/2)*grow*chngsize),int(scrnh/10+(-abs(i-speed/2)+speed/2)*grow*chngsize)))
+            screen.blit(self.pc, (oldx + xf - (-abs(i-speed/2)+speed/2)*grow*chngsize/2, oldy + yf - (-abs(i-speed/2)+speed/2)*grow*chngsize/2))
+            pygame.display.update()
+            if i == speed:
+                screen.blit(board, (xx, yy))
+                screen.blit(self.pc, (self.x, self.y))
+                pygame.display.update()
+                break
+            i = i + 1
+    
     def select(self):
         self.selected = not self.selected
 
@@ -174,42 +337,103 @@ class piece():
             fix = 1
         xset = int((self.x-xx)/size)*size
         yset = int((self.y-yy)/size)*size
+        oldx = self.x
+        oldy = self.y
+        boolean = False
         if not self.king:
-            print(yset, xset, int((y-yy)/size)*size, int((x-xx)/size)*size)
             if yset == int((y-yy)/size)*size+size*fix and xset == int((x-xx)/size)*size+size:
-                self.y = int((y-yy)/size)*size+yy
-                self.x = int((x-xx)/size)*size+xx
-                turn = turn + 1
-                return True
+                boolean = True
             elif yset == int((y-yy)/size)*size+size*fix and xset == int((x-xx)/size)*size-size:
-                self.y = int((y-yy)/size)*size+yy
-                self.x = int((x-xx)/size)*size+xx
-                turn = turn + 1
-                return True
+                boolean = True
         if self.king:
             if yset == int((y-yy)/size)*size-size*fix and xset == int((x-xx)/size)*size+size:
-                self.y = int((y-yy)/size)*size+yy
-                self.x = int((x-xx)/size)*size+xx
-                turn = turn + 1
-                return True
+                boolean = True
             elif yset == int((y-yy)/size)*size-size*fix and xset == int((x-xx)/size)*size-size:
-                self.y = int((y-yy)/size)*size+yy
-                self.x = int((x-xx)/size)*size+xx
-                turn = turn + 1
-                return True
-        return False
-        
+                boolean = True
+        if boolean:
+            self.y = int((y-yy)/size)*size+yy
+            self.x = int((x-xx)/size)*size+xx
+            self.yspot = int((y-yy)/size)
+            self.xspot = int((x-xx)/size)
+            if not self.king and ((self.yspot == 0 and self.color == 'black') or (self.yspot == 7 and self.color == 'red')):
+                self.makeKing()
+            turn = turn + 1
+            self.motion(oldx, oldy, False)
+        return boolean
+
+    def jumpValid(self, x, y):
+        global turn
+        minusturn = False
+        if self.color == 'red':
+            if turn % 2 == 0:
+                minusturn = True
+            fix = -1
+        if self.color == 'black':
+            if turn % 2 == 1:
+                minusturn = True
+            fix = 1
+        xset = int((self.x-xx)/size)*size
+        yset = int((self.y-yy)/size)*size
+        oldx = self.x
+        oldy = self.y
+        oldxspot = self.xspot
+        oldyspot = self.yspot
+        boolean = False
+        if yset == int((y-yy)/size)*size+size*fix*2 and xset == int((x-xx)/size)*size+size*2:
+            boolean = True
+        elif yset == int((y-yy)/size)*size+size*fix*2 and xset == int((x-xx)/size)*size-size*2:
+            boolean = True
+        if self.king:
+            if yset == int((y-yy)/size)*size-size*fix*2 and xset == int((x-xx)/size)*size+size*2:
+                boolean = True
+            elif yset == int((y-yy)/size)*size-size*fix*2 and xset == int((x-xx)/size)*size-size*2:
+                boolean = True
+        eatboolean = False
+        if boolean:
+            self.y = int((y-yy)/size)*size+yy
+            self.x = int((x-xx)/size)*size+xx
+            self.yspot = int((y-yy)/size)
+            self.xspot = int((x-xx)/size)
+        for checker in checkers:
+            if checker.color != self.color and (xset+xx < checker.getX() < self.x or xset+xx > checker.getX() > self.x) and (yset+yy < checker.getY() < self.y or yset+yy > checker.getY() > self.y):
+                checker.eat()
+                eatboolean = True
+                break
+        if not eatboolean:
+            print(self.x, self.y, self.xspot, self.yspot)
+            self.x = oldx
+            self.y = oldy
+            self.xspot = oldxspot
+            self.yspot = oldyspot
+            turn = turn - 1
+            boolean = False
+        else:
+            self.y = int((y-yy)/size)*size+yy
+            self.x = int((x-xx)/size)*size+xx
+            self.yspot = int((y-yy)/size)
+            self.xspot = int((x-xx)/size)
+            if not self.king and ((self.yspot == 0 and self.color == 'black') or (self.yspot == 7 and self.color == 'red')):
+                self.makeKing()
+            turn = turn + 1
+            self.motion(oldx, oldy, True)
+            if minusturn:
+                turn = turn - 1
+        return boolean
+
 wh = pygame.display.get_window_size()
 tempscrnw = int(wh[0])
 tempscrnh = int(wh[0]*9/16)
 load()
 pieces((scrnw-boardsize)/2,(scrnh-boardsize)/2)
+makeButtons()
 choice = None
 global xx
 global yy
 global turn
 turn = 0
 while True:
+    xx = (scrnw-boardsize)/2
+    yy = (scrnh-boardsize)/2
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -221,14 +445,16 @@ while True:
             tempscrnw = scrnw
             tempscrnh = scrnh
             root.state('zoomed')
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
- 
-    xx = (scrnw-boardsize)/2
-    yy = (scrnh-boardsize)/2
-    if xx <= mouse[0] <= xx+boardsize and yy <= mouse[1] <= yy+boardsize:
-        pyautogui.click(button='left')
-        if click[0] == 1:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
+            for i in buttons:
+                if i.getX() <= mouse[0] < i.getX()+size and i.getY() <= mouse[1] < i.getY()+size:
+                    choice = i
+                    for j in buttons:
+                        if j.getSelect() and j != choice:
+                            j.select()
+                    i.select()
+                    i.do()
             for i in checkers:
                 if i.getX() <= mouse[0] < i.getX()+size and i.getY() <= mouse[1] < i.getY()+size:
                     choice = i
@@ -236,9 +462,10 @@ while True:
                         if j.getSelect() and j != choice:
                             j.select()
                     i.select()
-            if choice != None and choice.getSelect() and not(choice.getX() <= mouse[0] < choice.getX()+size and choice.getY() <= mouse[1] < choice.getY()+size):
-                print(choice.valid(mouse[0], mouse[1]))
-
+            if choice != None and choice.getSelect() and abs(choice.xspot - (int((mouse[0]-xx)/size))) == 2 and abs(choice.yspot - (int((mouse[1]-yy)/size))) == 2:
+                choice.jumpValid(mouse[0], mouse[1])
+            elif choice != None and choice.getSelect() and not(choice.getX() <= mouse[0] < choice.getX()+size and choice.getY() <= mouse[1] < choice.getY()+size):
+                choice.valid(mouse[0], mouse[1])
     wh = pygame.display.get_window_size()
     scrnw = int(wh[0])
     scrnh = int(wh[0]*9/16)
@@ -247,7 +474,6 @@ while True:
     background()
     updatePieces((scrnw-boardsize)/2,(scrnh-boardsize)/2)
     pygame.display.update()
-    
 
 
     
