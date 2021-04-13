bb3f9f4fccd74352f3407ea3b8c242227494cc89
jjd358 <2662-jjd358@users.noreply.gitlab.cci.drexel.edu>
2021-04-13 00:14:34 +0000

    Update Basic_Neural_Network.py

--- code/NN/Basic_Neural_Network.py
+++ code/NN/Basic_Neural_Network.py
@@ -11,34 +11,43 @@ import pandas as pd
 from tensorflow.keras import datasets, layers, models
 
 data=[
-      [1,1,1,1],
-      [1,1,1,1],
-      [1,1,1,1],
-      [0,0,0,0],
-      [0,0,0,0],
-      [-1,-1,-1,-1],
-      [-1,-1,-1,-1],
-      [-1,-1,-1,-1]
+      1,1,1,1,
+      1,1,1,1,
+      1,1,1,1,
+      0,0,0,0,
+      0,0,0,0,
+      -1,-1,-1,-1,
+      -1,-1,-1,-1,
+      -1,-1,-1,-1
+      ]
+data2=[
+      1,1,1,1,
+      1,1,1,1,
+      1,1,1,1,
+      0,0,0,0,
+      0,0,0,0,
+      -1,-1,-1,-1,
+      -1,-1,-1,-1,
+      -1,-1,-1,-1
       ]
 
-
 label=[
-      [1,1,1,1],
-      [1,1,1,1],
-      [1,1,1,1],
-      [0,0,0,0],
-      [0,0,0,-1],
-      [-1,-1,-1,0],
-      [-1,-1,-1,-1],
-      [-1,-1,-1,-1]
+      1,1,1,1,
+      1,1,1,1,
+      1,1,1,1,
+      0,0,0,0,
+      0,0,0,1,
+      1,1,1,0,
+      1,1,1,1,
+      1,1,1,1
       ]
 
 
 
 model = models.Sequential()
-model.add(tf.keras.Input(shape=(8,4)))
-model.add(layers.Dense(32,activation='relu'))
-model.add(tf.keras.layers.Dense(4))
+model.add(tf.keras.Input(shape=(32,1),name="Input"))
+model.add(layers.Dense(8,activation='relu'))
+model.add(tf.keras.layers.Dense(32))
 
 
 
@@ -46,8 +55,11 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(),
               metrics=['accuracy'])
 
-history = model.fit(data, label, epochs=1)
+history = model.fit(data, label, epochs=5)
 
+
+predictions = model.predict(data2)
+print(len(predictions))
 """
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
